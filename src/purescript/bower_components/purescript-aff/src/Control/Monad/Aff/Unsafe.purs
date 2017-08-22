6a9bef8003a7f02dcf711c5ad335c9e5cd318ddf
Max New <maxsnew@gmail.com>
2017-08-22 18:54:03 -0400

    Site updated: Tue Aug 22 18:54:03 EDT 2017

--- src/purescript/bower_components/purescript-aff/src/Control/Monad/Aff/Unsafe.purs
+++
@@ -1,7 +0,0 @@
-module Control.Monad.Aff.Unsafe where
-
-import Control.Monad.Aff (Aff)
-import Unsafe.Coerce (unsafeCoerce)
-
-unsafeCoerceAff :: forall e1 e2 a. Aff e1 a -> Aff e2 a
-unsafeCoerceAff = unsafeCoerce
