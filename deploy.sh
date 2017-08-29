70171bbc07be0f071ff84451d4cdf459e5daffa4
Max New <maxsnew@gmail.com>
2017-08-29 19:05:09 -0400

    try something

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-stack exec maxsnew-exe clean
-    && stack exec maxsnew-exe build
-    && git checkout master
-    && cp -a _site/. .
-    && git add -A
-    && git commit -m "Site updated: $(date)"
-    && git push origin master
+stack exec maxsnew-exe clean \
+    && stack exec maxsnew-exe build \
+    && git checkout master \
+    && cp -a _site/. . \
+    && git add -A \
+    && git commit -m "Site updated: $(date)" \
+    && git push origin master \
     && git checkout src
