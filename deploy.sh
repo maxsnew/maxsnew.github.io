db4cd889186f32b0c971ece58edc9306f396e7d7
Max New <maxsnew@gmail.com>
2017-08-29 10:44:10 -0400

    update deploy based on gh-pages tutorial

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-stack exec maxsnew-exe build && git checkout master && git rm -rf --ignore-unmatch * && mv -f _site/* . && rm -rf _site && git add . && git commit -m "Site updated: $(date)" && git push origin master && git checkout src
+stack exec maxsnew-exe clean
+    && stack exec maxsnew-exe build
+    && git checkout master
+    && cp -a _site/. .
+    && git add -A
+    && git commit -m "Site updated: $(date)"
+    && git push origin master
+    && git checkout src
