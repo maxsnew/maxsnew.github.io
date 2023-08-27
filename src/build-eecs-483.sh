20fb2a22a42c291a3b86b2d7cd2db5b71686eef7
Max New <maxsnew@gmail.com>
2023-08-27 15:55:16 -0400

    init 438 fa23

--- src/build-eecs-483.sh
+++ src/build-eecs-483.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DEST=`pwd`/content/teaching/eecs-483-fa22
+DEST=`pwd`/content/teaching/eecs-483-fa23
 
-cd src/483fa22 && make web && chmod +w HTML_WEB/* && cp HTML_WEB/* $DEST
+cd src/483-fa23 && make web && chmod +w HTML_WEB/* && cp HTML_WEB/* $DEST
