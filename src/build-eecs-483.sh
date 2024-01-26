d4410f4248dc6a997b4db57576085eeeedb1eca5
Max New <maxsnew@gmail.com>
2024-01-26 09:32:31 -0500

    483

--- src/build-eecs-483.sh
+++ src/build-eecs-483.sh
@@ -3,5 +3,4 @@
 DEST=`pwd`/content/teaching/eecs-483-wn24
 
 # cd src/483-fa23 && make web && chmod +w HTML_WEB/* && cp HTML_WEB/* $DEST
-cd src/483-wn24/web && make html && cp -r build/html/* $DEST
-
+cd src/483-wn24/web && make html && cp -r build/html/* $DEST && rm -rf $DEST/_sources
